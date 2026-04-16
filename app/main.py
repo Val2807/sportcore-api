8afa75895ae3a19415d83bd6d1eb252011a5791d
Val2807 <valentin.nikonorov00@mail.ru>
2026-04-16 14:08:46 +0300

    created main file

--- app/main.py
+++ app/main.py
@@ -1,46 +1,39 @@
 from datetime import date
-
 from app.db import engine, Base, SessionLocal
-from app.models.player import Player
+from app.services.player_service import create_player, get_player, delete_player
 
 
 Base.metadata.create_all(bind=engine)
 
 session = SessionLocal()
 
-existing_player = (
-    session.query(Player)
-    .filter_by(
-        first_name="Ivan",
-        last_name="Petrov",
-        birth_date=date(2010, 5, 1),
-        team_id=1
-    )
-    .first()
-)
+first_name = "Ivan"
+last_name = "Ivanov"
+birth_date = date(2008, 5, 1)
+team_id = 1
+position = "forward"
+weight: 50.4
 
-players = session.query(Player).all()
-for player in players:
-    print(player.first_name, player.last_name)
+player = create_player(session, first_name, last_name,
+                       birth_date, team_id, position, weight)
 
+if player:
+    print(f"Player {player.first_name} {player.last_name} was created")
 
-if existing_player:
-    print("Player already exists")
+found_player = get_player(session, first_name, last_name, birth_date, team_id,)
+
+if found_player:
+    print(
+        f"Player {found_player.first_name} {found_player.last_name} was founded")
 else:
-    new_player = Player(
-        first_name="Ivan",
-        last_name="Petrov",
-        birth_date=date(2010, 5, 1),
-        team_id=1, 
-        weight=42.5
-    )
+    print("Player not found")
 
-    session.add(new_player)
-    session.commit()
-    print("Player saved successfully")
+deleted_player = delete_player(
+    session, first_name, last_name, birth_date, team_id,)
 
 
-if existing_player:
-    session.delete(existing_player)
-    session.commit()
-    print("Player deleted")
+if player:
+    print(
+        f"Player {deleted_player.first_name} {deleted_player.last_name} was deleted")
+else:
+    print("Player not found")
