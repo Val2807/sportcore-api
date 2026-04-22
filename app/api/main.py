8206bf5b70366c2819c2aff5d8d15fb4d8127986
Val2807 <valentin.nikonorov00@mail.ru>
2026-04-22 15:10:45 +0300

    added post create player

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,11 @@
-from app.services.player_service import get_all_players
-from app.db import SessionLocal
+from datetime import date
+
 from fastapi import FastAPI
+
+from app.api.schemas import PlayerCreate
+from app.db import SessionLocal
+from app.services.player_service import create_player, get_all_players
+
 
 app = FastAPI()
 
@@ -15,3 +20,28 @@ def get_players():
     session = SessionLocal()
     players = get_all_players(session)
     return players
+
+
+@app.post("/players")
+def create_player_endpoint(player: PlayerCreate):
+    session = SessionLocal()
+
+    new_player = create_player(
+        session=session,
+        first_name=player.first_name,
+        last_name=player.last_name,
+        birth_date=date.fromisoformat(player.birth_date),
+        team_id=player.team_id,
+        position=player.position,
+        weight=player.weight,
+    )
+
+    return {
+        "id": new_player.id,
+        "first_name": new_player.first_name,
+        "last_name": new_player.last_name,
+        "birth_date": str(new_player.birth_date),
+        "team_id": new_player.team_id,
+        "position": new_player.position,
+        "weight": new_player.weight,
+    }
