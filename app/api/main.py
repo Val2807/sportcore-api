77e14e187a69eb8ee80c01f3781f9d2bdebdc282
Val2807 <valentin.nikonorov00@mail.ru>
2026-05-05 12:13:30 +0300

    refactored api layer, created router players

--- app/api/main.py
+++ app/api/main.py
@@ -1,85 +1,13 @@
-from datetime import date
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Response, HTTPException, Depends
-
-
-from app.api.schemas import PlayerCreate, PlayerWeightUpdate, PlayerResponse
-from app.db import get_db
-
-from app.services.player_service import create_player, get_all_players, get_player_by_id, serialize_player, update_player_weight_by_id, delete_player_by_id
-
-from sqlalchemy.orm import Session
+from app.api.routers import players
 
 
 app = FastAPI()
+
+app.include_router(players.router)
 
 
 @app.get("/")
 def root():
     return {"message": "SportCore API is running"}
-
-
-@app.get("/players", response_model=list[PlayerResponse])
-def get_players(
-    team_id: int | None = None,
-    position: str | None = None,
-    db: Session = Depends(get_db)
-):
-    players = get_all_players(
-        session=db,
-        team_id=team_id,
-        position=position
-    )
-    return players
-
-
-@app.post("/players", response_model=PlayerResponse)
-def create_player_endpoint(player: PlayerCreate, db: Session = Depends(get_db)):
-
-    new_player = create_player(
-        session=db,
-        first_name=player.first_name,
-        last_name=player.last_name,
-        birth_date=date.fromisoformat(player.birth_date),
-        team_id=player.team_id,
-        position=player.position,
-        weight=player.weight,
-    )
-
-    return serialize_player(new_player)
-
-
-@app.get("/players/{player_id}", response_model=PlayerResponse)
-def get_player_by_id_endpoint(player_id: int, db: Session = Depends(get_db)):
-
-    found_player_by_id = get_player_by_id(db, player_id)
-    if not found_player_by_id:
-        raise HTTPException(status_code=404, detail="Player not found")
-    return serialize_player(found_player_by_id)
-
-
-@app.patch("/players/{player_id}/weight", response_model=PlayerResponse)
-def update_player_weight_endpoint(player_id: int, data: PlayerWeightUpdate, db: Session = Depends(get_db)):
-
-    updated_player = update_player_weight_by_id(
-        session=db,
-        player_id=player_id,
-        weight=data.weight,
-    )
-
-    if not updated_player:
-        raise HTTPException(status_code=404, detail="Player not found")
-
-    return serialize_player(updated_player)
-
-
-@app.delete("/players/{player_id}")
-def delete_player_by_id_endpoint(player_id: int, db: Session = Depends(get_db)):
-
-    deleted_player = delete_player_by_id(
-        session=db,
-        player_id=player_id
-    )
-    if not deleted_player:
-        raise HTTPException(status_code=404, detail="Player not found")
-    return Response(status_code=204)
